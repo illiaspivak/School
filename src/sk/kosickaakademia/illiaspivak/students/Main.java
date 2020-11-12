9cbf279d42a9963d7f9121643336c4390c6ded81
illiaspivak <illia.spivak@kosickaakademia.sk>
2020-11-12 10:19:55 +0100

    Ukazat studentov na konsole

--- src/sk/kosickaakademia/illiaspivak/students/Main.java
+++ src/sk/kosickaakademia/illiaspivak/students/Main.java
@@ -1,12 +1,41 @@
 package sk.kosickaakademia.illiaspivak.students;
 
+import sk.kosickaakademia.illiaspivak.school.Student;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    try{
+    public static void main(String[] args) {
+        try{ //определяет блок кода, в котором может произойти исключение
+            FileReader fr = new FileReader("resourse/students.dat"); //открыть файл
+            String line; //количество строк в файле
+            BufferedReader bf = new BufferedReader(fr); // читает текст из потока ввода символов
+            line=bf.readLine(); // считывает сколько строк в файле (это указано в первой строке
+            int num =  Integer.parseInt(line); //переоброзовал кодличество строк в int
+            System.out.println("Файл содержит " + num + " строк");
+            Students [] array = new Students[num]; //создаем массив студентов
+            for (int i=0;i<num;i++){
+                line=bf.readLine(); 
+                String[] temp = line.split(" "); // пробел разбивает наши слова на отдельные обьекты
+                Students s = new Students(temp[0],temp[1],temp[2]); //разбивает строку на три отдельные слова
+                array[i]=s; //пременную s вложили в массив
+            }
+            printAllStudents(array); //вызываем функцию вывода студентов на консоль
+
+            fr.close(); // закрываем файл, который считывали
 
 
-    }catch(IOException ex){
-        ex.printStackTrace();
+        }catch(IOException ex){ //определяет блок кода, в котором происходит обработка исключения;
+            ex.printStackTrace(); //помогает понять, где на самом деле возникла проблема
+        }
+    }
+
+    public static void printAllStudents (Students[] array){ //метод, который выводт в консоль всех студентов
+        int len = array.length;
+        for(int i=0; i<len; i++){
+            System.out.println(array[i].getFirsName() + " " + array[i].getLastName());
+        }
     }
 }
