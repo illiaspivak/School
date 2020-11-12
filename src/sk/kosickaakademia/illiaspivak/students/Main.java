b647364b71a0044e07df38fdd72cdce2e2ac821a
illiaspivak <illia.spivak@kosickaakademia.sk>
2020-11-12 21:36:17 +0100

    Pridal hmotnost a vysku studentov

--- src/sk/kosickaakademia/illiaspivak/students/Main.java
+++ src/sk/kosickaakademia/illiaspivak/students/Main.java
@@ -19,7 +19,9 @@ public class Main {
             for (int i=0;i<num;i++){
                 line=bf.readLine();
                 String[] temp = line.split(" "); // пробел разбивает наши слова на отдельные обьекты
-                Students s = new Students(temp[0],temp[1],temp[2]); //разбивает строку на три отдельные слова
+                double height = Double.parseDouble(temp[3]); // из строки вытягиваем данные про рост
+                int weight = Integer.parseInt(temp[4]); //из строки вытягиваем информацию про массу
+                Students s = new Students(temp[0],temp[1],temp[2],height,weight); //разбивает строку на три отдельные слова
                 array[i]=s; //пременную s вложили в массив
             }
             printAllStudents(array); //вызываем функцию вывода студентов на консоль
@@ -37,7 +39,7 @@ public class Main {
         System.out.println("Vsetky studenty:");
         int len = array.length;
         for(int i=0; i<len; i++){
-            System.out.println("     " + array[i].getFirsName() + " " + array[i].getLastName());
+            System.out.println("     " + array[i].getFirsName() + " " + array[i].getLastName() + " " + array[i].getHeight());
         }
     }
     public static void printAllWomans (Students[] array){ //метод, который выводт в консоль всех студентов
